ac5799ed035cd7cd626f7fae6c533d40090c36a7
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-23 13:30:04 +0800

    Edited the cmd commands to be able to run in powershell

--- Execution/T1059.003.ps1
+++ Execution/T1059.003.ps1
@@ -5,13 +5,16 @@ Set-Content -Path $env:TEMP\T1059.003_script.bat -Value "dir"
 
 Start-Process $env:TEMP\T1059.003_script.bat
 
+
 #Atomic Test 2 - Writes text to a file and displays it.
-echo "Hello from the Windows Command Prompt!" > "%TEMP%\test.bin" & type "%TEMP%\test.bin"
+Start-Process -Filepath cmd.exe -ArgumentList '/c "echo "Hello from the Windows Command Prompt!" > "%TEMP%\test.bin" & type "%TEMP%\test.bin"'
 #cleanup command:
-del "%TEMP%\test.bin" >nul 2>&1
+Start-Process -Filepath cmd.exe -ArgumentList '/c "del "%TEMP%\test.bin" >nul 2>&1"'
+
 
 #Atomic Test 3 - Suspicious Execution via Windows Command Shell
-%LOCALAPPDATA:~-3,1%md /c echo Hello, from CMD! > #hello.txt & type #hello.txt
+Start-Process -Filepath cmd.exe -ArgumentList '/c "%LOCALAPPDATA:~-3,1%md /c echo Hello, from CMD! > #hello.txt & type #hello.txt"'
+
 
 #Atomic Test 4 - Simulate BlackByte Ransomware Print Bombing
 new-item $env:temp\T1059_003note.txt -value "This file has been created by T1059.003 Test 4" -Force | Out-Null
@@ -19,8 +22,9 @@ cmd /c "for /l %x in (1,1,75) do start wordpad.exe /p $env:temp\T1059_003note.tx
 #cleanup command:
 stop-process -name wordpad -force -erroraction silentlycontinue
 
+
 #Atomic Test 5 - Command Prompt read contents from CMD file and execute
-New-Item -Type Directory (split-path C:\AtomicRedTeam\atomics -ErrorAction ignore | Out-Null
+New-Item -Type Directory (split-path C:\AtomicRedTeam\atomics) -ErrorAction ignore | Out-Null
 Invoke-WebRequest "https://github.com/redcanaryco/atomic-red-team/raw/master/atomics/T1059.003/src/t1059.003_cmd.cmd" -OutFile "#{input_file}"
 
 cmd /r cmd<C:\AtomicRedTeam\atomics\T1059.003\src\t1059.003_cmd.cmd
