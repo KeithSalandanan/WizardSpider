3e14cc7cd09a9e684e731ec38499025f155e1689
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-23 13:19:07 +0800

    Edited the T1059.003 Test 5

--- Execution/T1059.003.ps1
+++ Execution/T1059.003.ps1
@@ -13,10 +13,14 @@ del "%TEMP%\test.bin" >nul 2>&1
 #Atomic Test 3 - Suspicious Execution via Windows Command Shell
 %LOCALAPPDATA:~-3,1%md /c echo Hello, from CMD! > #hello.txt & type #hello.txt
 
-#Atomic Test 4 - Windows Command Shell
+#Atomic Test 4 - Simulate BlackByte Ransomware Print Bombing
 new-item $env:temp\T1059_003note.txt -value "This file has been created by T1059.003 Test 4" -Force | Out-Null
 cmd /c "for /l %x in (1,1,75) do start wordpad.exe /p $env:temp\T1059_003note.txt" | out-null
 #cleanup command:
 stop-process -name wordpad -force -erroraction silentlycontinue
 
-#Atomic Test 5 - Windows Command Shell
+#Atomic Test 5 - Command Prompt read contents from CMD file and execute
+New-Item -Type Directory (split-path C:\AtomicRedTeam\atomics -ErrorAction ignore | Out-Null
+Invoke-WebRequest "https://github.com/redcanaryco/atomic-red-team/raw/master/atomics/T1059.003/src/t1059.003_cmd.cmd" -OutFile "#{input_file}"
+
+cmd /r cmd<C:\AtomicRedTeam\atomics\T1059.003\src\t1059.003_cmd.cmd
