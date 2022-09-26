ada8f74d3fb17cdd0d93b3928771b7098106af54
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-26 14:01:40 +0800

    Edited T1112

--- Persistence/T1112.ps1
+++ Persistence/T1112.ps1
@@ -1,5 +1,5 @@
 #Atomic Test #1 - Modify Registry of Current User Profile - cmd
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v HideFileExt /d 1 /f
+Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /t REG_DWORD /v HideFileExt /d 1 /f"'
 
 #Cleanup Commands:
-#reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /f >nul 2>&1
+#Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /f >nul 2>&1"'
