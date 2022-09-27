17b4ef204137d1c7a965ee68c3f89a4100a6b007
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-27 12:27:17 +0800

    Edited T1053.005, T1204.002, T1569.002

--- Execution/T1053.005.ps1
+++ Execution/T1053.005.ps1
@@ -6,22 +6,3 @@ Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "schtasks /create
 #Cleanup Commands:
 # Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "schtasks /delete /tn "T1053_005_OnLogon" /f >nul 2>&1"'
 # Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "schtasks /delete /tn "T1053_005_OnStartup" /f >nul 2>&1"'
-
-#Atomic Test #2 - Scheduled task Local
-
-# SCHTASKS /Create /SC ONCE /TN spawn /TR #{task_command} /ST #{time}
-
-#Cleanup Commands:
-# SCHTASKS /Delete /TN spawn /F >nul 2>&1
-
-#Atomic Test #4 - Powershell Cmdlet Scheduled Task
-
-# $Action = New-ScheduledTaskAction -Execute "calc.exe"
-# $Trigger = New-ScheduledTaskTrigger -AtLogon
-# $User = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
-# $Set = New-ScheduledTaskSettingsSet
-# $object = New-ScheduledTask -Action $Action -Principal $User -Trigger $Trigger -Settings $Set
-# Register-ScheduledTask AtomicTask -InputObject $object
-
-#Cleanup Commands:
-# Unregister-ScheduledTask -TaskName "AtomicTask" -confirm:$false >$null 2>&1
--- Execution/T1569.002.ps1
+++ Execution/T1569.002.ps1
@@ -1,9 +1,9 @@
 #Atomic Test 1 - Execute a Command as a Service
 
-sc.exe create ARTService binPath= "%COMSPEC% /c powershell.exe -nop -w hidden -command New-Item -ItemType File C:\art-marker.txt"
-sc.exe start ARTService
-sc.exe delete ARTService
+Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "sc.exe create ARTService binPath= "%COMSPEC% /c powershell.exe -nop -w hidden -command New-Item -ItemType File C:\art-marker.txt"'
+Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "sc.exe start ARTService"'
+Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "sc.exe delete ARTService"'
 
 #Cleanup Commands:
-#del C:\art-marker.txt >nul 2>&1
+#Start-Process -Filepath cmd.exe  -Verb runAs -ArgumentList '/c "del C:\art-marker.txt >nul 2>&1"'
 
