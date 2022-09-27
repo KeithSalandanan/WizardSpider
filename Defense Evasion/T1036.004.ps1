edb99fa6848a109b61cb11a736f5b0a8512b037d
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-27 14:33:16 +0800

    fixed T1036.004

--- Defense Evasion/T1036.004.ps1
+++ Defense Evasion/T1036.004.ps1
@@ -1,7 +1,7 @@
 #Atomic Test #1 - Creating W32Time similar named service using schtasks
 
-Start-Process -FilePath cmde.exe -Verb -RunAs -ArgumentList '/c "schtasks /create /ru system /sc daily /tr "cmd /c powershell.exe -ep bypass -file c:\T1036.004_NonExistingScript.ps1" /tn win32times /f"'
-Start-Process -FilePath cmde.exe -Verb -RunAs -ArgumentList '/c "schtasks /query /tn win32times"'
+Start-Process -FilePath cmde.exe -Verb runAs -ArgumentList '/c "schtasks /create /ru system /sc daily /tr "cmd /c powershell.exe -ep bypass -file c:\T1036.004_NonExistingScript.ps1" /tn win32times /f"'
+Start-Process -FilePath cmde.exe -Verb runAs -ArgumentList '/c "schtasks /query /tn win32times"'
 
 # Cleanup Commands:
-# Start-Process -FilePath cmde.exe -Verb -RunAs -ArgumentList '/c "schtasks /tn win32times /delete /f"'
+# Start-Process -FilePath cmde.exe -Verb runAs -ArgumentList '/c "schtasks /tn win32times /delete /f"'
