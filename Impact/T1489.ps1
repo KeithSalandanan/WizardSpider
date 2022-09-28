3296d377c1afed450772b876ca6e3fb2d8ec77df
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-28 16:56:53 +0800

    edited T1489

--- Impact/T1489.ps1
+++ Impact/T1489.ps1
@@ -1,4 +1,9 @@
 #Atomic Test #2 - Windows - Stop service using net.exe
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
 
 net.exe stop spooler
 
