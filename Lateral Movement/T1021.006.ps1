ad510e091d110542a747fce960d5edd08744d51c
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-28 14:49:10 +0800

    added T1021.006

---
+++ Lateral Movement/T1021.006.ps1
@@ -0,0 +1,8 @@
+#Atomic Test #1 - Enable Windows Remote Management
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process PowerShell -Verb RunAs "-NoProfile -ExecutionPolicy Bypass -Command `"cd '$pwd'; & '$PSCommandPath';`"";
+    exit;
+}
+
+Enable-PSRemoting -Force
