c0db5cd88e1b23b16e18db9d26b35c0d36474f99
keith.salandanan <keith.salandanan@oqulo.com>
2022-09-28 14:36:45 +0800

    edited T1033

--- Discovery/T1033.ps1
+++ Discovery/T1033.ps1
@@ -1,10 +1,10 @@
 #Atomic Test #1 - System Owner/User Discovery
 
-cmd.exe /C whoami
-cmd.exe wmic useraccount get /ALL
-cmd.exe quser /SERVER:"#{computer_name}"
-cmd.exe quser
-cmd.exe qwinsta.exe /server:#{computer_name}
-cmd.exe qwinsta.exe
-cmd.exe for /F "tokens=1,2" %i in ('qwinsta /server:#{computer_name} ^| findstr "Active Disc"') do @echo %i | find /v "#" | find /v "console" || echo %j > computers.txt
-cmd.exe @FOR /F %n in (computers.txt) DO @FOR /F "tokens=1,2" %i in ('qwinsta /server:%n ^| findstr "Active Disc"') do @echo %i | find /v "#" | find /v "console" || echo %j > usernames.txt
+Start-Process -Filepath cmd.exe -ArgumentList '/c "cmd.exe /C whoami"'
+Start-Process -Filepath cmd.exe -ArgumentList '/c "wmic useraccount get /ALL"'
+Start-Process -Filepath cmd.exe -ArgumentList '/c "quser /SERVER:"#{computer_name}""'
+Start-Process -Filepath cmd.exe -ArgumentList '/c "quser"'
+Start-Process -Filepath cmd.exe -ArgumentList '/c "qwinsta.exe /server:#{computer_name}"'
+Start-Process -Filepath cmd.exe -ArgumentList '/c "qwinsta.exe"'
+Start-Process -Filepath cmd.exe -ArgumentList '/c "for /F `tokens=1,2` %i in ("qwinsta /server:#{computer_name} ^| findstr "Active Disc"") do @echo %i | find /v "#" | find /v "console" || echo %j > computers.txt"'
+Start-Process -Filepath cmd.exe -ArgumentList '/c "@FOR /F %n in (computers.txt) DO @FOR /F "tokens=1,2" %i in ("qwinsta /server:%n ^| findstr "Active Disc"") do @echo %i | find /v "#" | find /v "console" || echo %j > usernames.txt"'
